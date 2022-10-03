3993ae4a56d6191965f6215d6b1a0a598ffeb91c
VitoriaCaminha <vitoria@praciano.com.br>
2022-10-03 14:19:23 -0300

    fix: second project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Dev from './images/vitoriacaminha.png'
 import FundamentosJS from './images/fundamentos-js.png'
-import Calculadora from './images/calculadora.js'
-import { Presentation, Projects } from './Style'
+import Calculadora from './images/calculadora.png'
+import { Box, Presentation, Projects } from './Style'
 
 export default function App() {
   return (
@@ -15,16 +15,21 @@ export default function App() {
         </div>
       </Presentation>
 
-      <Projects>
-        <a href='https://fundamentos-js.vercel.app' target='_blank'>
-          Fundamentos JavaScript
-          <img src={FundamentosJS} alt='imagem do projeto chamado Fundamentos JavaScript' />
-        </a>
-        <a href='https://calculadora.vitoriacaminha.vercel.app' target='_blank'>
-          <img src={Calculadora} alt='imagem do projeto chamado Calculadora' />
-          Calculadora
-        </a>
-      </Projects>
+      <Box>
+        <Projects>
+          <a href='https://fundamentos-js.vercel.app' target='_blank'>
+            Fundamentos JavaScript
+            <img src={FundamentosJS} alt='imagem do projeto chamado Fundamentos JavaScript' />
+          </a>
+        </Projects>
+
+        <Projects>
+          <a href='https://calculadora.vitoriacaminha.vercel.app' target='_blank'>
+            <img src={Calculadora} alt='imagem do projeto chamado Calculadora' />
+            Calculadora
+          </a>
+        </Projects>
+      </Box>
     </div>
   );
 }
--- src/Style.js
+++ src/Style.js
@@ -3,25 +3,31 @@ import styled from 'styled-components'
 export const Presentation = styled.div`
 @import url('https://fonts.googleapis.com/css2?family=Yeseva+One&display=swap');
 font-family: 'Yeseva One', 'cursive';
-background-color: aliceblue;
+background-color: #daeafd;
 height: 60vh;
 display: flex;
 justify-content: space-evenly;
 align-items: center;
 
 img {
-    width: 200px;
-    height: 200px;
+    width: 30vh;
     border-radius: 50%;
 }
 `
 
+export const Box = styled.div`
+padding: 2vh 0;
+`
+
 export const Projects = styled.div`
 width: 60vw;
-margin: auto;
+margin: 2vh auto;
+border: solid 1px gray;
+border-radius: 5px;
 
 a {
     text-decoration: none;
+    font-size: 5vh;
     color: black;
     display: flex;
     justify-content: space-around;
